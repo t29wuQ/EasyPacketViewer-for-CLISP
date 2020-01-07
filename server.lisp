871f5a6127051395cb6a2a637e7be2a966ad2913
K.Tsukimoto <t.29.wuq@gmail.com>
2020-01-07 22:52:38 +0900

    Refactoring

--- server.lisp
+++ server.lisp
@@ -3,41 +3,38 @@
     (unwind-protect 
         (loop (with-open-stream (stream (socket-accept socket))
             (let ((access (parse-method-path (read-line stream))))
-                    (router stream (car access) (cdr access)))
-            ;; (parse-method-path (read-line stream))
-            ;; (format stream (concatenate 'string (create-http-header) (get-packet-html)))
-            )
+                    (router stream (car access) (cdr access))))
     ) (socket-server-close socket))))
 
 
-(defun parse-method-path (str) 
+(defun parse-method-path (str) ;methodとpathを取得
     (let ((method (subseq str  0 (position #\space str))) ;method (GET POST)
           (path (subseq str (+ 2 (position #\space str)) (position #\space str :from-end t)))) ;path
     (return-from parse-method-path (cons method path)) ))
 
-(defun create-http-header () 
+(defun create-http-header ()  ;http headerを返すだけ
     (return-from create-http-header "HTTP/1.1 200 ~%Content-Type: text/html;~2% "))
 
-(defun router (stream method path) 
+(defun router (stream method path) ;pathから処理関数を選択
     (cond ((equal path "packet")  
             (res-packet stream method))))
 
-(defun res-packet (stream method)
-    (cond ((equal method "GET")
+(defun res-packet (stream method) ;/pathの処理
+    (cond ((equal method "GET") ;GET
             (format stream (concatenate 'string (create-http-header) (get-html "packet.html"))))
-            ((equal method "POST") 
+            ((equal method "POST") ;POST
                 (loop (cond ((equal (read-line stream) "Content-Disposition: form-data; name=\"data\"")
                             (read-line stream)
                             (format stream (concatenate 'string "HTTP/1.1 200 ~%Content-Type: application/json;~2%" (packet-parser (read-line stream))))
                             (return-from res-packet))))
                 ) ))
 
-(defun get-html (name) 
-    (with-open-file (stream name :direction :input) ;packet.htmlを読み込んで返す
+(defun get-html (name) ;html読み込む  name : filename
+    (with-open-file (stream name :direction :input) 
         (let ((buf (make-string (file-length stream))))
             (read-sequence buf stream) (return-from get-html buf))))
 
-(defun packet-parser (packet) 
+(defun packet-parser (packet) ;packetのパーサー jsonにして返す
     (let ((json (make-string-output-stream))
             (header-length (subseq packet 1 2))
             (protocol (subseq packet 18 20)))
@@ -93,7 +90,7 @@
         (princ "}" json)
         (return-from packet-parser (get-output-stream-string json))))
 
-(defun convert-ip-format (raw-address) 
+(defun convert-ip-format (raw-address) ;ipアドレスを見やすい形に加工 
     (return-from convert-ip-format 
         (concatenate 'string (write-to-string (parse-integer (subseq raw-address 0 2) :radix 16)) 
                     "." 
@@ -103,5 +100,4 @@
                     "." 
                     (write-to-string (parse-integer (subseq raw-address 7 8) :radix 16)))))
 
-;; (print (packet-parser "45000054C56C0000400133E9C0A80002C0A80001000092921E02000225C8C74C7251050008090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303132333435363"))
 (server)
